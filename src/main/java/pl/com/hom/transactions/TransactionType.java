cec3027c12f33c54c7ef5f99431cc8eb63093b70
Marek Szweda <mszweda93@gmail.com>
2020-03-11 21:00:31 +0100

    Dodanie encji dla typu tranzakcji.

---
+++ src/main/java/pl/com/hom/transactions/TransactionType.java
@@ -0,0 +1,76 @@
+package pl.com.hom.transactions;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TransactionType implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false)
+	private String type;
+
+	@Column(nullable = false)
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof TransactionType))
+			return false;
+		TransactionType other = (TransactionType) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Transaction type [id=" + id + ", name=" + name + ", type =" + type + "]";
+	}
+}
