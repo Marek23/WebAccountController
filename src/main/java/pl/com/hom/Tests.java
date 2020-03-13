5fcd093057c132b538308040231322567a4a1c81
Marek Szweda <mszweda93@gmail.com>
2020-03-13 19:56:09 +0100

    Działająca wersja z kluczami.
    Usunięte toString() i usunięte ręczne nadawanie nazw tabelom.
    Hibernate sam inicjalizuje schemat bazy danych.

--- src/main/java/pl/com/hom/Tests.java
+++ src/main/java/pl/com/hom/Tests.java
@@ -2,6 +2,7 @@ package pl.com.hom;
 
 import pl.com.hom.transactions.TransactionEntity;
 import pl.com.hom.transactions.TransactionType;
+import pl.com.hom.transactions.Transaction;
 
 import static pl.com.hom.connection.SessionCreator.saveToDB;
 import static pl.com.hom.connection.SessionCreator.init;
@@ -18,8 +19,16 @@ public class Tests {
 		t.setName("test name");
 		t.setType("TYPE____");
 
+		Transaction out = new Transaction();
+		out.setDescription("test");
+		out.setExecDate("20190101");
+		out.setOrderDate("20190101");
+		out.setTransactionEntity(e);
+		out.setTransactionType(t);
+
 		saveToDB(e);
 		saveToDB(t);
+		saveToDB(out);
 
 		System.out.println("Stop");
 	}
---
+++ src/main/java/pl/com/hom/transactions/Transaction.java
@@ -0,0 +1,100 @@
+package pl.com.hom.transactions;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+public class Transaction implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int    id;
+	private String execDate;
+	private String orderDate;
+	private String description;
+
+	@ManyToOne()
+	private TransactionEntity transactionEntity;
+
+	@ManyToOne()
+	private TransactionType transactionType;
+
+	public String getExecDate() {
+		return execDate;
+	}
+
+	public void setExecDate(String execDate) {
+		this.execDate = execDate;
+	}
+
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public TransactionEntity getTransactionEntity() {
+		return transactionEntity;
+	}
+
+	public void setTransactionEntity(TransactionEntity transactionEntity) {
+		this.transactionEntity = transactionEntity;
+	}
+
+	public void setTransactionType(TransactionType transactionType) {
+		this.transactionType = transactionType;
+	}
+
+	public TransactionType getTransactionType() {
+		return transactionType;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Transaction))
+			return false;
+		Transaction other = (Transaction) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+}
